e2b8883bd6d3835291ecb206cd3759a881cc46f6
T0mmyliu <liuweipeng0001@gmail.com>
2017-05-22 23:47:30 +0800

    tmp save

--- raw_example/epoll.cpp
+++ raw_example/epoll.cpp
@@ -4,16 +4,20 @@
 
 
 #include <iostream>
+#include <sys/socket.h>
 #include <glog/logging.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
   google::InitGoogleLogging(argv[0]);
+  google::SetLogDestination(google::GLOG_INFO,"../LOG/epoll/raw_epoll");
 
-  google::SetLogDestination(google::GLOG_INFO,"./LOG/epoll/raw_epoll");
 
-  LOG(INFO) << "Hello,GLOG!";
+
+
+
+
 
   return 0;
 }
